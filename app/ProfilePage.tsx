d4572fde9fa40c8a3e178755e801bc080392e943
mookil <mikael2aa@gmail.com>
2025-10-18 00:09:23 -0600

    profile page pretty much done
    - implemented all sections from the snapshots, minus the "need help" section
    - some things i couldn't figure out were how they aligned the items in the buy again section, and some centering/aligning stuff
    - all images take from the picsum API website

--- app/ProfilePage.tsx
+++ app/ProfilePage.tsx
@@ -1,10 +1,15 @@
+import ListsCard from '@/components/ProfilePageComponents/ListsCard';
+import OrderCard from '@/components/ProfilePageComponents/OrderCard';
 import ProfilePageCard from '@/components/ProfilePageComponents/ProfilePageCard';
+import SmallerCard from '@/components/ProfilePageComponents/SmallerCard';
+import WideCard from '@/components/ProfilePageComponents/WideCard';
 import { buyAgainProps, keepShoppingProps, listsRegisteriesProps, subscribeSaveProps, yourOrdersProps } from '@/lib/products';
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import SimpleLineIcons from '@expo/vector-icons/SimpleLineIcons';
 import React from 'react';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 const ProfilePage = () => {
   return (
@@ -24,14 +29,29 @@ const ProfilePage = () => {
         <View style={styles.profileHeaderSection2}>
           <Ionicons name="settings-outline" size={24} color="black" style={styles.pHSection2Elements} />
           <FontAwesome5 name="bell" size={24} color="black" style={styles.pHSection2Elements} />
+          <View style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            paddingBottom: 10,
+          }}>
+            <Image source={require('../assets/images/flags/flag_CA.png')}
+            style={{width: 30, height: 30}}/>
           <Text>EN</Text>
+          </View>
+          
         </View>
       </View>
       
       {/* Widgets Section */}
-      <View>
-
-      </View>
+      <ScrollView
+        horizontal 
+        showsHorizontalScrollIndicator={false}
+        style={styles.widgetsContainer}>
+        <Text style={styles.widget}>Orders</Text>
+        <Text style={styles.widget}>Buy Again</Text>
+        <Text style={styles.widget}>Account</Text>
+        <Text style={styles.widget}>Lists</Text>
+      </ScrollView>
 
       {/* User Orders section */}
       <View>
@@ -46,10 +66,7 @@ const ProfilePage = () => {
         showsHorizontalScrollIndicator={false}
         style={styles.cardContainer}>
           {yourOrdersProps.map((props, index) => (
-            <ProfilePageCard pageProps={props} key={index}/>
-          )) }
-          {yourOrdersProps.map((props, index) => (
-            <ProfilePageCard pageProps={props} key={index}/>
+            <OrderCard pageProps={props} key={index}/>
           )) }
         </ScrollView>
         
@@ -82,14 +99,12 @@ const ProfilePage = () => {
         </View>
         
         {/* Product Cards */}
-        <ScrollView 
-        horizontal 
-        showsHorizontalScrollIndicator={false}
-        style={styles.cardContainer}>
-          {subscribeSaveProps.map((props, index) => (
-            <ProfilePageCard pageProps={props} key={index}/>
+          <View>
+            {subscribeSaveProps.map((props, index) => (
+            <WideCard pageProps={props} key={index}/>
           )) }
-        </ScrollView>
+          
+          </View>
       </View>
 
       {/* Keep shopping for items section */}
@@ -100,14 +115,31 @@ const ProfilePage = () => {
         </View>
         
         {/* Product Cards */}
-        <ScrollView 
-        horizontal 
-        showsHorizontalScrollIndicator={false}
-        style={styles.cardContainer}>
+        <View style={styles.overflowCardContainer}>
           {keepShoppingProps.map((props, index) => (
-            <ProfilePageCard pageProps={props} key={index}/>
+            <SmallerCard pageProps={props} key={index}/>
           )) }
-        </ScrollView>
+
+          {/* See more and Edit section */}
+        </View>
+        <View style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          padding: 15,
+          paddingTop: 0,
+          
+        }}>
+          <View style={{
+            flexDirection: 'row',
+            justifyContent: 'center',
+            gap: 5
+          }}>
+            <SimpleLineIcons name="arrow-down" size={11} color="#3c76ab" />
+            <Text style={{
+              color: '#3c76ab'}}>See More</Text>
+          </View>
+          <Text style={{color: '#3c76ab'}}>Edit</Text>
+        </View>
       </View>
 
       {/* Lists + Registeries section */}
@@ -118,23 +150,50 @@ const ProfilePage = () => {
         </View>
         
         {/* Product Cards */}
-        <ScrollView 
-        horizontal 
-        showsHorizontalScrollIndicator={false}
-        style={styles.cardContainer}>
-          {listsRegisteriesProps.map((props, index) => (
-            <ProfilePageCard pageProps={props} key={index}/>
+        <View>
+            {listsRegisteriesProps.map((props, index) => (
+            <ListsCard pageProps={props} key={index}/>
           )) }
-        </ScrollView>
+          
+          </View>
       </View>
 
       {/* Account settings section */}
       <View>
         <View style={styles.headerSection}>
           <Text style={styles.headerText}>Your Accounts</Text>
+          <FontAwesome5 name="arrow-right" size={20} color="black" />
         </View>
-        
+
+        <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+         style={styles.widgetsContainer}>
+            <Text style={styles.widget}>Your Orders</Text>
+            <Text style={styles.widget}>Your Payments</Text>
+            <Text style={styles.widget}>Your Subscribe & Save</Text>
+          </ScrollView>
       </View>
+
+      <View style={{paddingTop: 20}}></View>
+
+      {/* Gift Card section */}
+      <View>
+        <View style={styles.headerSection}>
+          <Text style={styles.headerText}>Gift Card Balance: $0.00</Text>
+          <FontAwesome5 name="arrow-right" size={20} color="black" />
+        </View>
+
+        <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+         style={styles.widgetsContainer}>
+            <Text style={styles.widget}>Your Orders</Text>
+            <Text style={styles.widget}>Your Payments</Text>
+          </ScrollView>
+      </View>
+
+      <View style={{paddingTop: 200}}></View>
     </ScrollView>
   )
 }
@@ -157,7 +216,7 @@ const styles = StyleSheet.create({
       flexDirection: 'row',
       width: '15%',
       gap: 15,
-      marginRight: 20,
+      marginRight: 50,
       justifyContent: 'space-between',
     },
     profilePicture: {
@@ -188,11 +247,34 @@ const styles = StyleSheet.create({
     },
     headerText: {
       fontWeight: 'bold',
-      fontSize: 20,
+      fontSize: 23,
     }, 
     cardContainer: {
       flexDirection: 'row',
       width: '100%',
+    },
+    overflowCardContainer: {
+      flexDirection:'row',
+      width: '100%',
+      flexWrap: 'wrap',
+
+    },
+    widgetsContainer: {
+      flexDirection: 'row',
+      padding: 15,
+      gap: 5,
+    },
+    widget: {
+      borderWidth: 2,
+      borderRadius: 15,
+      minWidth: 110,
+      padding: 10,
+      paddingLeft: 15,
+      paddingRight: 15,
+      borderColor: '#a9b7b7',
+      marginRight: 5,
+      textAlign: 'center',
+
     }
 
 })
